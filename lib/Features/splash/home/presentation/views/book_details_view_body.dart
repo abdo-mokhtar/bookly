8e10d0ee7d4e058d93621c4265f656e075f8a1f6
Abdo Mokhtar <abdoshahbe@gmail.com>
2024-12-03 13:07:08 +0200

    create book details

---
+++ lib/Features/splash/home/presentation/views/book_details_view_body.dart
@@ -0,0 +1,11 @@
+import 'package:book/Features/splash/home/presentation/views/book_details_views.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Features/splash/home/presentation/views/book_details_views.dart
@@ -0,0 +1,13 @@
+import 'package:book/Features/splash/home/presentation/views/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsViews extends StatelessWidget {
+  const BookDetailsViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
